1e7d9beef529fc6cf6c369b12ab5ae8d7f443c98
franballerio <129407745+franballerio@users.noreply.github.com>
2023-09-18 13:42:23 -0300

    segunda parte

--- guia5/ej2.hs
+++ guia5/ej2.hs
@@ -45,3 +45,54 @@ eliminarOnce [] y = []
 eliminarOnce (x:xs) y 
  | x == y = xs 
  | otherwise = x:(eliminarOnce xs y) 
+
+ -- 6) 
+quitarTodos :: (Eq t) => [t] -> t -> [t]
+quitarTodos [] y = []
+quitarTodos (x:xs) y 
+ | x == y = quitarTodos xs y 
+ | otherwise = x:(quitarTodos xs y) 
+
+-- 7) 
+eliminarRepetidos :: Eq t => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:[]) = [x]
+eliminarRepetidos (x:xs) 
+ | elem x xs = eliminarRepetidos xs
+ | otherwise = x:(eliminarRepetidos xs) 
+
+
+-- 8) 
+mismosElementos :: Eq t => [t] -> [t] -> Bool
+mismosElementos [] [] = True
+mismosElementos (x:[]) [] = False
+mismosElementos [] (y:[]) = False
+mismosElementos (x:xs) [] = False
+mismosElementos [] (y:ys) = False
+mismosElementos (x:[]) (y:[]) 
+ | x == y = True
+ | otherwise = False
+mismosElementos (x:xs) (y:[]) = False
+mismosElementos (x:[]) (y:ys) = False
+mismosElementos (x:xs) (y:ys)
+ | x == y = mismosElementos xs ys
+ | otherwise = False
+
+
+capicua :: Eq t => [t] -> Bool
+capicua x = x == reverso x
+
+
+reverso :: Eq t => [t] -> [t]
+reverso [] = []
+reverso xs = ultimo xs : (reverso xs)
+
+ultimo :: [t] -> t
+ultimo (x:[]) = x
+ultimo (x:xs) = ultimo (xs)
+
+eliminarElem :: (Eq t) => [t] -> t -> [t]
+eliminarElem [] y = []
+eliminarElem (x:xs) y 
+ | x == y = eliminarElem xs y 
+ | otherwise = x:(eliminarElem xs y) 
